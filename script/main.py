83cb0c81e2f3bed7c1fe15c5db4bf866530e60ea
Victor Hugo <victormagalhaes00@outlook.com>
2019-08-30 01:06:06 -0300

    Update main.py

--- script/main.py
+++ script/main.py
@@ -2,31 +2,20 @@
 
 #Imports das UI's
 from Ui_logged import *
-from resources_mat_rc import *
-
-#Imports das bibliotecas: geral
 from PyQt5.QtCore import *
 import time
 import platform
 import sys
-
-####################### End of Imports ########################
-
    
-class ThreadSignal(QObject): 
+class ThreadSignal(QObject): #Signal class (you better use it on your code)
         sig = pyqtSignal(str) 
+      
 
-class webThread(QThread): #Thread de leitura dos crachás
+class webThread(QThread): 
     send2thread_signal = ThreadSignal()
-
-    def signal_test(self, url):
-        print('sinal emitido')
-        self.signal_login.emit(url) 
+      
     
     def run(self):
-        global logged
-        global objLogged
-
         while(True):
                 link ='https://www.google.com.br'
                 self.send2thread_signal.sig.emit(str(link))
@@ -37,12 +26,9 @@ class webThread(QThread): #Thread de leitura dos crachás
                 link ='https://www.twitter.com'
                 self.send2thread_signal.sig.emit(str(link))
                 time.sleep(10)
+                  
 
-
-
-   
-
-#Classe que herda as propriedades da classe Ui_Logged e gerencia a formação e os eventos da janela inicial
+#Class to generate the main Window
 class Logged(QtWidgets.QMainWindow, Ui_Logged):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
@@ -51,27 +37,22 @@ class Logged(QtWidgets.QMainWindow, Ui_Logged):
                 
     def load_url(self, url):
         self.web.setUrl(QUrl(url))
+         
 
-
-
-
-#Função principal que inicia as funcionalidades e o programa
 def main():
         app = QtWidgets.QApplication([])
-
         global logged
         logged = QtWidgets.QMainWindow()
         global objLogged
         objLogged = Logged()
         objLogged.setupUi(logged)        
         logged.show()
-
         thread = webThread()
         thread.start()
+        app.exec_() #GUI main loop
+        thread.terminate() #thread end
 
-        app.exec_() #loop principal de excução da GUI
-        thread.terminate() #fim da thread com o fim do programa (apenas prevenção, não se espera que o programa termine)
-
+      
 if __name__ == "__main__":
         main()
 
